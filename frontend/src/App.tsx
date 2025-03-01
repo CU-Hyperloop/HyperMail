f76bb978b04486432879de136f6c5fe73f44010c
Harika <harikakondur3@gmail.com>
2025-03-01 15:09:01 -0700

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
+import {BrowserRouter as Router, Route, Routes,  } from 'react-router';
 
 export default function App() {
   return (
   <MantineProvider>
-    {/* Your app here */}
-    <div>hello hypermail !!</div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>hello hypermail !!!</div>} />
+      </Routes>
+    </Router>
     
   </MantineProvider>
   
